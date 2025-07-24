3dc3bab275fbb7f2c362e1e987734a77be9785b2
Shubham Singh Prajapati <siddharthsingh75250@gmail.com>
2025-07-24 22:34:14 +0530

    new

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -2,13 +2,16 @@ import {
   ChartLineIcon,
   CircleDollarSignIcon,
   PlayCircleIcon,
+  StarIcon,
   UserIcon,
 } from "lucide-react";
-import React, { useEffect, useState } from "react";
-import { dummyDashboardData } from "../../assets/assets";
+import React, { useEffect, useState, } from "react";
+import { dummyDashboardData,} from "../../assets/assets";
 import Loading from "../../components/Loading";
 import Title from "../../components/admin/Title";
 import BlurCircle from "../../components/BlurCircle";
+// import timeFormat from "../lib/timeFormat";
+import { dateFormat } from "../../lib/dateFormat";
 
 const Dashboard = () => {
   const currency = import.meta.env.VITE_CURRENCY;
@@ -29,7 +32,7 @@ const Dashboard = () => {
     },
     {
       title: "Total Revenue",
-      value: dashboardData.totalRevenue || "0",
+      value: currency+dashboardData.totalRevenue || "0",
       icon: CircleDollarSignIcon,
     },
     {
@@ -59,8 +62,36 @@ const Dashboard = () => {
       <div className="relative flex flex-wrap gap-4 mt-6">
         <BlurCircle top="100px" left="0"/>
         <div className="flex flex-wrap gap-4 w-full "> 
+          {dashboardCards.map((card,index)=>(
+            <div className="flex items-center justify-between px-4 py-3 bg-primary/10 border border-primary/20 rounded-md max-w-50 w-full">
+              <div>
+              <h1 className="text-sm">{card.title}</h1>
+              <p className="text-xl font-medium mt-1 ">{card.value}</p>
+            </div>
+            <card.icon className="w-6 h-6"/>
+            </div>
+          ))}
 
         </div>
+      </div>
+      <p className="mt-10 text-lg font-medium">Active Shows</p>
+      <div className="mt-4 max-w-5xl flex flex-wrap gap-6 relative">
+          <BlurCircle top="100px" left="-10%"/>
+          {dashboardData.activeShows.map((show)=>(
+            <div key={show._id} className=" w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1
+            transition duration-300">
+              <img src={show.movie.poster_path} alt="" className="h-60 w-full object-cover" />
+              <p className="font-medium p-2 truncate">{show.movie.title}</p>
+              <div className="flex items-center justify-between px-2">
+                <p className="text-lg font-medium">{currency} {show.showPrice}</p>
+                <p className="felx items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+                  <StarIcon className="w-4 h-4 text-primary fill-primary"/>
+                  {show.movie.vote_average.toFixed(1)}
+                </p>
+              </div>
+              <p className="px-2 pt-2 text-sm text-gray-500">{dateFormat(show.showDateTime)}</p>
+            </div>
+          ))}
       </div>
     </>
   ) : (
--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Loading from "../../components/Loading";
+import Title from "../../components/admin/Title";
 
 const ListShows = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const currency = import.meta.env.VITE_CURRENCY;
 
-export default ListShows
+  const [shows, setShows] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getAllShows = async () => {
+    try {
+      setShows([
+        {
+          movie: dummyShowsData[0],
+          showDataTime: "2025-06-30T02:30:00.000Z",
+          showPrice: 59,
+          occupiedSeats: {
+            A1: "user_1",
+            B1: "user_2",
+            C1: "user_3",
+          },
+        },
+      ]);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getAllShows();
+  }, []);
+
+  return !loading ?(
+    <>
+      <Title text1="Admin" text2="Dashboard" />
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default ListShows;
